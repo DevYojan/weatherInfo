d847431e58668847fdfa22fab6766c96ba290b38
Yojan Regmi <devyojan@gmail.com>
2020-03-04 23:08:43 +0400

    Completed initial forecast

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,10 @@ import ForecastList from './ForecastList';
 import forecast from '../apis/forecast';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      errMessage: null,
-      weatherData: null,
-    }
+  state = {
+    errMessage: null,
+    weatherData: null,
+    forecastData: null,
   }
 
   componentDidMount() {
@@ -22,53 +19,55 @@ class App extends React.Component {
         errMessage: 'OOps... 😞 Your device does not support geolocation. This app can\'t function without geolocation.',
       });
     } else {
+
+      // For current weather
       navigator.geolocation.getCurrentPosition(async (position) => {
         const query = `?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${API_KEY}`;
-        // let response = await currentweather.get(query);
-        // response = response.data;
-        let response = {
-          "coord": {
-            "lon": 55.95,
-            "lat": 25.78
-          },
-          "weather": [
-            {
-              "id": 801,
-              "main": "Clouds",
-              "description": "few clouds",
-              "icon": "02n"
-            }
-          ],
-          "base": "stations",
-          "main": {
-            "temp": 19.92,
-            "feels_like": 19.43,
-            "temp_min": 18,
-            "temp_max": 21.67,
-            "pressure": 1013,
-            "humidity": 55
-          },
-          "visibility": 10000,
-          "wind": {
-            "speed": 1,
-            "deg": 160
-          },
-          "clouds": {
-            "all": 19
-          },
-          "dt": 1582135808,
-          "sys": {
-            "type": 1,
-            "id": 7544,
-            "country": "AE",
-            "sunrise": 1582080545,
-            "sunset": 1582121486
-          },
-          "timezone": 14400,
-          "id": 291074,
-          "name": "Ras Al Khaimah City",
-          "cod": 200
-        }
+        let response = await currentweather.get(query);
+        response = response.data;
+        // let response = {
+        //   "coord": {
+        //     "lon": 55.95,
+        //     "lat": 25.78
+        //   },
+        //   "weather": [
+        //     {
+        //       "id": 801,
+        //       "main": "Clouds",
+        //       "description": "few clouds",
+        //       "icon": "02n"
+        //     }
+        //   ],
+        //   "base": "stations",
+        //   "main": {
+        //     "temp": 19.92,
+        //     "feels_like": 19.43,
+        //     "temp_min": 18,
+        //     "temp_max": 21.67,
+        //     "pressure": 1013,
+        //     "humidity": 55
+        //   },
+        //   "visibility": 10000,
+        //   "wind": {
+        //     "speed": 1,
+        //     "deg": 160
+        //   },
+        //   "clouds": {
+        //     "all": 19
+        //   },
+        //   "dt": 1582135808,
+        //   "sys": {
+        //     "type": 1,
+        //     "id": 7544,
+        //     "country": "AE",
+        //     "sunrise": 1582080545,
+        //     "sunset": 1582121486
+        //   },
+        //   "timezone": 14400,
+        //   "id": 291074,
+        //   "name": "Ras Al Khaimah City",
+        //   "cod": 200
+        // }
 
         const weatherData = {
           description: response.weather[0].description,
@@ -83,35 +82,41 @@ class App extends React.Component {
 
         this.setState({
           weatherData
+        }, async () => {
+          const query = `?lat=${this.state.weatherData.lat}&lon=${this.state.weatherData.lon}&units=metric&appid=${API_KEY}`;
+          const response = await forecast.get(query);
+          this.setState({
+            forecastData: response.data,
+          });
         });
 
       });
     }
   }
 
-  async componentDidUpdate() {
-    const query = `?lat=${this.state.weatherData.lat}&lon=${this.state.weatherData.lon}&units=metric&appid=${API_KEY}`;
-    const response = await forecast.get(query);
-    console.log(response.data);
-  }
+  // async componentDidUpdate() {
+  //   const query = `?lat=${this.state.weatherData.lat}&lon=${this.state.weatherData.lon}&units=metric&appid=${API_KEY}`;
+  //   const response = await forecast.get(query);
+  //   this.setState({
+  //     forecastData: response.data,
+  //   });
+  // }
 
   render() {
 
     if (!this.state.weatherData) {
       return (
-      <div className="container loading">
-        <h1>Loading ...</h1>
-        <img src={loadingGif} alt="Loading..."/>
-      </div>);
+        <div className="container loading">
+          <h1>Loading ...</h1>
+          <img src={loadingGif} alt="Loading..." />
+        </div>);
     }
-
-    console.log(this.state.weatherData);
 
     return (
       <div className="container">
         <h1 className="heading">Weather Info</h1>
         <TodaysWeather weatherData={this.state.weatherData} />
-        <ForecastList/>
+        <ForecastList forecastData={this.state.forecastData} />
       </div>
     );
   }
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,8 +1,30 @@
+import '../forecast.css';
+import {amOrpm} from '../helpers/helper.js';
 import React from 'react';
 
-const Forecast = () => {
+const Forecast = ({ data }) => {
+  const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  let time = new Date(data.dt * 1000);
+  time = amOrpm(time.getUTCHours());
+
   return (
-    <div>Forecast Card</div>
+    <div className="weatherCard">
+
+      <div className="timeAndLogo">
+        <img src={icon} alt={data.weather[0].description} />
+        <h3 className="time">{time}</h3>
+      </div>
+
+      <div className="info">
+        <h3>{data.main.temp} &deg;C</h3>
+        <p className="description">{data.weather[0].description} outside</p>
+        <div className="temps">
+          <p className="range"><strong>min:</strong> {data.main.temp_min} &deg;C</p>
+          <p className="range"><strong>max:</strong> {data.main.temp_max} &deg;C</p>
+        </div>
+      </div>
+
+    </div>
   );
 }
 
